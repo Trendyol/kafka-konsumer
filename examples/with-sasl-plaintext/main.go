a17f9479411b61b57b5b096c0dff9c25221f26f4
Elif Seray Dönmez <elifseraydonmez@gmail.com>
2023-06-13 16:09:36 +0300

    docs: add sasl/plain example

---
+++ examples/with-sasl-plaintext/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+
+	"github.com/Trendyol/kafka-konsumer"
+)
+
+func main() {
+	consumerCfg := &kafka.ConsumerConfig{
+		Concurrency: 1,
+		Reader: kafka.ReaderConfig{
+			Brokers: []string{"localhost:29093"},
+			Topic:   "standard-topic",
+			GroupID: "standard-cg",
+		},
+		RetryEnabled: false,
+		ConsumeFn:    consumeFn,
+		SASL: &kafka.SASLConfig{
+			Type:     kafka.MechanismPlain,
+			Username: "client",
+			Password: "client-secret",
+		},
+		TLS:  nil,
+		Rack: "rack1",
+	}
+
+	consumer, _ := kafka.NewConsumer(consumerCfg)
+	defer consumer.Stop()
+
+	consumer.Consume()
+
+	fmt.Println("Consumer started...!")
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	<-c
+}
+
+func consumeFn(message kafka.Message) error {
+	fmt.Printf("Message From %s with value %s", message.Topic, string(message.Value))
+	return nil
+}
