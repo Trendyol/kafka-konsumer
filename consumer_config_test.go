45f00fc5c9dd0ca296ac243d85e2dcc545f758f8
Abdulsametileri <sametileri07@gmail.com>
2023-09-17 14:02:54 +0300

    test: add consumer config validate method tests

---
+++ consumer_config_test.go
@@ -0,0 +1,42 @@
+package kafka
+
+import (
+	"testing"
+	"time"
+)
+
+func TestConsumerConfig_validate(t *testing.T) {
+	t.Run("Set_Defaults", func(t *testing.T) {
+		// Given
+		cfg := ConsumerConfig{Reader: ReaderConfig{}}
+
+		// When
+		cfg.validate()
+
+		// Then
+		if cfg.Concurrency != 1 {
+			t.Fatalf("Concurrency default value must equal to 1")
+		}
+		if cfg.CommitInterval != time.Second {
+			t.Fatalf("Commit Interval default value must equal to 1s")
+		}
+		if cfg.Reader.CommitInterval != time.Second {
+			t.Fatalf("Reader Commit Interval default value must equal to 1s")
+		}
+	})
+	t.Run("Set_Commit_Interval_Value_To_The_Internal_Reader", func(t *testing.T) {
+		// Given
+		cfg := ConsumerConfig{CommitInterval: 5 * time.Second, Reader: ReaderConfig{}}
+
+		// When
+		cfg.validate()
+
+		// Then
+		if cfg.CommitInterval != 5*time.Second {
+			t.Fatalf("Commit Interval value must equal to 5s")
+		}
+		if cfg.Reader.CommitInterval != 5*time.Second {
+			t.Fatalf("Reader Commit Interval default value must equal to 5s")
+		}
+	})
+}
