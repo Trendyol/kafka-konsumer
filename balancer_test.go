a1afb707451e5bfef485db997ea0a79fb04c5b53
pinarrkok <pinarkok07@gmail.com>
2025-04-07 18:51:43 +0300

    feat: add given, when, then for balancer unit tests

--- balancer_test.go
+++ balancer_test.go
@@ -121,20 +121,30 @@ func TestDefaultBalancer_Balance(t *testing.T) {
 	partitions := []int{0, 1, 2, 3}
 
 	t.Run("Should_Use_RoundRobin_When_Key_Is_Nil", func(t *testing.T) {
+		// Given
 		msg := kafka.Message{Key: nil}
 		balancer := &DefaultBalancer{}
 		expected := GetBalancerRoundRobin().Balance(msg, partitions...)
+
+		// When
 		result := balancer.Balance(msg, partitions...)
+
+		// Then
 		if result != expected {
 			t.Errorf("Expected RoundRobin partition %d, got %d", expected, result)
 		}
 	})
 
 	t.Run("Should_Use_Murmur2_When_Key_Is_Not_Nil", func(t *testing.T) {
+		// Given
 		msg := kafka.Message{Key: []byte("key")}
 		balancer := &DefaultBalancer{}
 		expected := GetBalancerMurmur2Balancer().Balance(msg, partitions...)
+
+		// When
 		result := balancer.Balance(msg, partitions...)
+
+		// Then
 		if result != expected {
 			t.Errorf("Expected Murmur2Balancer partition %d, got %d", expected, result)
 		}
